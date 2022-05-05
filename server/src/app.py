3d8af8f823c982a78fbe914afbb2e62869392aff
janmoerdeligero <janmoerdeligero@gmail.com>
2022-05-05 21:24:54 +0800

    initial commit

--- server/src/app.py
+++ server/src/app.py
@@ -1,88 +1,11 @@
-from flask import Flask, jsonify, request, make_response, g
-from models.dog import Dog
-from models.cat import Cat
-from db import Db
+from flask import Flask, Blueprint
+from v1.dog.router import DogRouter
 
 app = Flask(__name__)
 
-#g.db = Db()
-#g.db.connect()
-
-@app.route('/')
-def home():
-    return "Hello Flask!"
-
-@app.route('/dogs', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def dogs():
-    dog = Dog()
-    if str(request.method).upper() == 'POST':
-        resp = dog.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(dog.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(dog.delete(request.json))
-    dogs = dog.get()
-    return jsonify(dogs)
-
-@app.route('/dogs/<dog_id>', methods=['GET', 'PUT', 'DELETE'])
-def dog(dog_id):
-    dog_object = Dog()
-    dog = dog_object.get({"id": dog_id})
-    if dog is None:
-        return make_response(jsonify({"error": "Dog id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        dog_data = request.json
-        dog_data['id'] = dog_id
-        resp = dog_object.put(dog_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(dog_object.delete(dog_id))
-    return jsonify(dog)
-
-@app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def cats():
-    cat = Cat()
-    if str(request.method).upper() == 'POST':
-        resp = cat.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(cat.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat.delete(request.json))
-    cats = cat.get()
-    return jsonify(cats)
-
-@app.route('/cats/<cat_id>', methods=['GET', 'PUT', 'DELETE'])
-def cat(cat_id):
-    cat_object = Cat()
-    cat = cat_object.get({"id": cat_id})
-    if cat is None:
-        return make_response(jsonify({"error": "cat id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        cat_data = request.json
-        cat_data['id'] = cat_id
-        resp = cat_object.put(cat_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat_object.delete(cat_id))
-    return jsonify(cat)
+bp_dogs = Blueprint('dogs', __name__, url_prefix='/v1/dogs')
+DogRouter.handler(bp_dogs)
+app.register_blueprint(bp_dogs)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
---
+++ server/src/v1/cat/cat.py
@@ -0,0 +1,69 @@
+from flask import g
+from db import Db
+
+class Cat():
+    def sanitize(self, cats):
+        if not isinstance(cats, (list, tuple)):
+            cats = [cats]
+        clean_cats = []
+        for cat in cats:
+            if not isinstance(cat, dict):
+                continue
+            if not ('id' in cat and 'name' in cat):
+                continue
+            clean_cats.append(cat)
+        return clean_cats
+
+    def post(self, cats):
+        if not isinstance(cats, (list, tuple)):
+            cats = [cats]
+        clean_cats = self.sanitize(cats)
+        if len(cats) != len(clean_cats):
+            return False
+        queries = []
+        for cat in clean_cats:
+            sql = "INSERT INTO cats(name) VALUES(%s)"
+            queries.append({"sql": sql, "bind": cat['name']})
+        db = Db.get_instance()
+        result = db.transactional(queries)
+        return cats
+
+    def get(self, filters=None):
+        db = Db.get_instance()
+        if filters is not None:
+            if 'id' in filters:
+                sql = "SELECT * FROM cats WHERE id = %s"
+                cat = db.fetchone(sql, filters['id'])
+                return cat
+            # if another filter
+        sql = "SELECT * FROM cats ORDER BY name"
+        cats = db.fetchall(sql)
+        return cats
+
+    def put(self, cats):
+        if not isinstance(cats, (list, tuple)):
+            cats = [cats]
+        clean_cats = self.sanitize(cats)
+        if len(cats) != len(clean_cats):
+            return False
+        queries = []
+        for cat in clean_cats:
+            sql = "UPDATE cats SET name = %s WHERE id = %s"
+            queries.append({"sql": sql, "bind": (cat['name'], cat['id'])})
+        db = Db.get_instance()
+        db.transactional(queries)
+        return cats
+
+    def delete(self, cats):
+        counter = 0
+        if not isinstance(cats, (list, tuple)):
+            cats = [cats]
+        placeholder = []
+        queries = []
+        for cat in cats:
+            placeholder.append('%s')
+        sql = "DELETE FROM cats WHERE id IN (" + ", ".join(placeholder) + ")"
+        queries.append({"sql": sql, "bind": cats})
+        db = Db.get_instance()
+        counter = db.transactional(queries)
+        return counter
